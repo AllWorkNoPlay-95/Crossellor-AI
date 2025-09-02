6e73c462732550c6e230a3a8e6a8f7742dd1748f
Samuele Mancuso <s.mancuso95@gmail.com>
2025-09-02 15:50:56 +0200

    Add `xellr.py` with basic import and download functions

---
+++ xellr.py
@@ -0,0 +1,18 @@
+import requests
+
+
+# region Import Data
+def import_products():
+    pass
+
+
+def import_orders():
+    pass
+
+
+def download_from_url(url):
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.content
+    else:
+        raise Exception(f"Failed to download from {url}")
